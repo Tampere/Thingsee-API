cff59ddcfe1d6a1323f1e65a019a4d596924352f
Antti Sand <antti.sand@tampere.fi>
2015-10-21 08:45:39 +0000

    Added migration and model for events

---
+++ database/migrations/2015_10_21_083016_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('sid', 10);
+            $table->double('val', 15, 8);
+            $table->integer('ts')->unsigned();
+            $table->integer('device_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('device_id')
+                ->references('id')->on('devices')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}

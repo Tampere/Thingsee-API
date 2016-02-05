c4b0281bf5aee9d3936b58fd3e1b01e57d15da9b
Antti Sand <antti.sand@tampere.fi>
2016-02-05 12:57:13 +0200

    Added all data to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,27 +22,32 @@
         <div class="container">
             <h1>ThingSee data API Dashboard</h1>
             
-            <div id="chart_div_1" style="float:left"></div><div id="chart_div_2" style="float: left"></div>
-            <div id="chart_div_3" style="clear: both;"></div>
+            <div id="chart_div_1" style="float:left"></div><div id="chart_div_2" style="float: left"></div><div id="chart_div_3" style="float: left"></div><div id="chart_div_4" style="float: left"></div>
+            <div id="chart_div_5" style="clear: both;"></div>
         </div>
 
         <script type="text/javascript">
         var valuesTemperature = [];
-        var valuesHumidity = [];
-        var currentTemp, currentHumidity;
+        var valuesLuminance = [];
+        var currentTemp, currentHumidity, currentPressure, currentLuminance;
         var currentTs;
         var chart_div_1 = document.getElementById('chart_div_1');
         var chart_div_2 = document.getElementById('chart_div_2');
         var chart_div_3 = document.getElementById('chart_div_3');
+        var chart_div_4 = document.getElementById('chart_div_4');
+        var chart_div_5 = document.getElementById('chart_div_5');
         var chartTemp = new google.visualization.Gauge(chart_div_1);
         var chartHumi = new google.visualization.Gauge(chart_div_2);
+        var chartPress = new google.visualization.Gauge(chart_div_3);
+        var chartLumi = new google.visualization.Gauge(chart_div_4);
         var formatter = new google.visualization.DateFormat({pattern: "dd'/'MM' 'HH':'mm"});
 
         (function(){
-            $.getJSON('http://www.avoindata.net/thingsee/v1/events?sensor=0x00060100', function(json) {
-                valuesTemperature = json;
-                currentTemp = parseFloat(json[0]['val']);
-                currentTs = new Date(parseInt(json[0]['ts']));
+            $.getJSON('http://www.avoindata.net/thingsee/v1/envs/2/data?sensor=0x00060100', function(json) {
+//            $.getJSON('v1/envs/2/data?sensor=0x00060100', function(json) {
+                valuesTemperature = json['data'];
+                currentTemp = parseFloat(json['data'][0]['sensor']['value']);
+                currentTs = new Date(parseInt(json['data'][0]['timestamp']));
 
                 var data = google.visualization.arrayToDataTable([
                   ['Label', 'Value'],
@@ -54,9 +59,9 @@
                 drawLineChart();
             });
 
-            $.getJSON('http://www.avoindata.net/thingsee/v1/events?sensor=0x00060200&limit=1', function(json) {
-                valuesHumidity = json;
-                currentHumidity = parseFloat(json[0]['val']);
+            $.getJSON('http://www.avoindata.net/thingsee/v1/envs/2/data?sensor=0x00060200&limit=1', function(json) {
+//            $.getJSON('v1/envs/2/data?sensor=0x00060200', function(json) {
+                currentHumidity = parseFloat(json['data'][0]['sensor']['value']);
 
                 var data = google.visualization.arrayToDataTable([
                   ['Label', 'Value'],
@@ -65,10 +70,37 @@
 
                 chartHumi.draw(data, optionsHumidity);
 
-            });            
+            });
+
+            $.getJSON('http://www.avoindata.net/thingsee/v1/envs/2/data?sensor=0x00060300', function(json) {
+//            $.getJSON('v1/envs/2/data?sensor=0x00060300', function(json) {
+                valuesLuminance = json['data'];
+                currentPressure = parseFloat(json['data'][0]['sensor']['value']);
+
+                var data = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    ['Pressure', currentPressure]
+                ]);
+
+                chartPress.draw(data, optionsPressure);
+
+            });
+
+            $.getJSON('http://www.avoindata.net/thingsee/v1/envs/2/data?sensor=0x00060400&limit=1', function(json) {
+//            $.getJSON('v1/envs/2/data?sensor=0x00060400&limit=1', function(json) {
+                currentLuminance = parseFloat(json['data'][0]['sensor']['value']);
+
+                var data = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    ['Luminance', currentLuminance]
+                ]);
+
+                chartLumi.draw(data, optionsLuminosity);
+
+            });
 
             var optionsTemp = {
-              width: 600, height: 420,
+              width: 300, height: 210,
               redFrom: 35, redTo: 50,
               yellowFrom:-50, yellowTo: -35,
                 yellowColor: "#4679BD",
@@ -78,7 +110,7 @@
             };
 
             var optionsHumidity = {
-              width: 600, height: 420,
+              width: 300, height: 210,
               redFrom: 90, redTo: 100,
               yellowFrom: 80, yellowTo: 90,
               yellowColor: "#4679BD",
@@ -86,21 +118,49 @@
               min: 0,
               max: 100
             };
+
+            var optionsPressure = {
+                width: 300, height: 210,
+                minorTicks: 5,
+                min: 0,
+                max: 1000
+            };
+
+            var optionsLuminosity = {
+                width: 300, height: 210,
+                minorTicks: 5,
+                min: 0,
+                max: 5000
+            };
         })();
 
         function drawLineChart()
         {
+            var values = [];
+            for(var i = 0, len = valuesTemperature.length; i < len; i++)
+            {
+                values.push([
+                        valuesTemperature[i]['timestamp'],
+                        valuesTemperature[i]['sensor']['value'],
+                        valuesLuminance[i]['sensor']['value'] / 100
+                ]);
+            }
             var data = new google.visualization.DataTable();
             data.addColumn('date', 'X');
             data.addColumn('number', 'Temperature');
-            $.each(valuesTemperature, function(key, val) {
-                var tmpDate = new Date(parseInt(val['ts']));
-                data.addRow([tmpDate, parseFloat(val['val'])]);
+            data.addColumn('number', 'Luminance (x100)');
+            $.each(values, function(key, val) {
+                var tmpDate = new Date(parseInt(val[0]));
+                data.addRow([tmpDate, parseFloat(val[1]), parseFloat(val[2])]);
             });
 
             formatter.format(data, 0);
 
             var options = {
+                chart: {
+                   title: 'Values charted'
+                },
+                height: 500,
                 hAxis: {
                   title: 'Timestamp',
                   gridlines: {
@@ -108,11 +168,11 @@
                   }
                 },
                 vAxis: {
-                  title: 'Temperature'
+                  title: 'Values',
                 }
             };
 
-            var chart = new google.visualization.LineChart(chart_div_3);
+            var chart = new google.visualization.LineChart(chart_div_5);
 
             chart.draw(data, options);
         }

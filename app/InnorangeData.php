ede7db4a666c059bdaa9504a9401b91d392b7fa3
Antti Sand <antti.sand@tampere.fi>
2016-03-23 15:58:46 +0200

    Korjattu vierailoiden määrän castaus integeriksi

--- app/InnorangeData.php
+++ app/InnorangeData.php
@@ -8,6 +8,7 @@ class InnorangeData extends Model
 {
     protected $fillable = ['measurement_point', 'timestamp', 'visitors'];
     protected $hidden = ['id', 'created_at', 'updated_at', 'measurement_point'];
+    protected $casts = ['visitors' => 'integer'];
 
     public function MeasurementPoint()
     {
